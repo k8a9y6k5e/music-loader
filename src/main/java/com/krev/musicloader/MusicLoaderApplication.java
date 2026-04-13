f97d494b1141b888752d609fdaefd4355822f032
kayke <kaykekk8965@gmail.com>
2026-04-13 16:49:57 -0300

    first commit

---
+++ src/main/java/com/krev/musicloader/MusicLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.krev.musicloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krev/musicloader/MusicLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krev.musicloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

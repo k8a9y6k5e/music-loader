4b6a6959437326a1145c6223443f4c83e2402b85
kayke <kaykekk8965@gmail.com>
2026-04-16 17:12:00 -0300

    fix
    fix the name used at map of auth

--- src/main/java/com/krev/musicloader/api/SpotifyAuthMap.java
+++ src/main/java/com/krev/musicloader/api/SpotifyAuthMap.java
@@ -3,7 +3,7 @@ package com.krev.musicloader.api;
 import lombok.Data;
 
 @Data
-public class SpotifyMap {
+public class SpotifyAuthMap {
     private String access_token;
     private String token_type;
     private int expires_in;

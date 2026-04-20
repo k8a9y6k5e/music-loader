97808b56e10819e2ca951c6f0ce012d8a1e971f0
kayke <kaykekk8965@gmail.com>
2026-04-20 15:40:02 -0300

    feat
    add the service to list all musics

--- src/main/java/com/krev/musicloader/music/MusicController.java
+++ src/main/java/com/krev/musicloader/music/MusicController.java
@@ -1,13 +1,16 @@
 package com.krev.musicloader.music;
-import com.krev.musicloader.music.dto.CreateMusicDTO;
+import com.krev.musicloader.music.dto.CreateMusicDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 @RestController
 @RequestMapping("/music")
@@ -17,8 +20,14 @@ public class MusicController {
     private MusicService service;
 
     @PostMapping
-    public ResponseEntity<MusicEntity> create(@RequestBody @Valid CreateMusicDTO dto) {
+    public ResponseEntity<MusicEntity> create(@RequestBody @Valid CreateMusicDto dto) {
         MusicEntity result = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<MusicEntity>> list(Pageable pageable) {
+        Page<MusicEntity> result = service.list(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }

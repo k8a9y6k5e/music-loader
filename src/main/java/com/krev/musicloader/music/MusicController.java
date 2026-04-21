f70dec90094f7a15c50ba484dd1f771dbcc6f97e
kayke <kaykekk8965@gmail.com>
2026-04-21 10:36:21 -0300

    feat
    add the controller to search the musics

--- src/main/java/com/krev/musicloader/music/MusicController.java
+++ src/main/java/com/krev/musicloader/music/MusicController.java
@@ -1,12 +1,8 @@
 package com.krev.musicloader.music;
 import com.krev.musicloader.music.dto.CreateMusicDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -29,5 +25,10 @@ public class MusicController {
     public ResponseEntity<Page<MusicEntity>> list(Pageable pageable) {
         Page<MusicEntity> result = service.list(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MusicEntity> search(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.search(id));
     }
 }

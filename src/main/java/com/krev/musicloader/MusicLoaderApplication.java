6cb57cb2c6fd4249d52571e42d199a93610b98ae
kayke <kaykekk8965@gmail.com>
2026-04-21 14:51:32 -0300

    feat
    add the created at and updated at inside the table

--- src/main/java/com/krev/musicloader/MusicLoaderApplication.java
+++ src/main/java/com/krev/musicloader/MusicLoaderApplication.java
@@ -2,8 +2,10 @@ package com.krev.musicloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MusicLoaderApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/krev/musicloader/music/MusicEntity.java
+++ src/main/java/com/krev/musicloader/music/MusicEntity.java
@@ -1,15 +1,18 @@
 package com.krev.musicloader.music;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "music")
+@EntityListeners(AuditingEntityListener.class)
 public class MusicEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +26,10 @@ public class MusicEntity {
 
     @Column(name = "artist", nullable = false)
     private String artist;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }

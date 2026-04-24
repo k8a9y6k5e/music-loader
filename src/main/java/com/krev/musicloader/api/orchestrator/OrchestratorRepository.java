750cc81d4a84b1ddd968ee859ed1145c58b1f31d
kayke <kaykekk8965@gmail.com>
2026-04-24 20:45:51 -0300

    feat
    add the entity, repository and seed to the orchestrator

---
+++ src/main/java/com/krev/musicloader/api/orchestrator/OrchestratorRepository.java
@@ -0,0 +1,6 @@
+package com.krev.musicloader.api.orchestrator;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrchestratorRepository extends JpaRepository<OrchestratorEntity, Long> {
+}
---
+++ src/main/resources/OrchestratorSeed.sql
@@ -0,0 +1,2 @@
+INSERT INTO orchestrator (id, name, priority,activity)
+    VALUES (1,"client1", 1, true), (2,"client2", 2, true);

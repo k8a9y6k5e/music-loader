70db8fae2ddd697e9648aa93fb63d6978cfbabe3
kayke <kaykekk8965@gmail.com>
2026-04-25 22:00:23 -0300

    fix
    seed correction

--- src/main/resources/OrchestratorSeed.sql
+++ src/main/resources/OrchestratorSeed.sql
@@ -1,2 +1,3 @@
-INSERT INTO orchestrator (id, name, priority,activity)
-    VALUES (1,"client1", 1, true), (2,"client2", 2, true);
+INSERT INTO orchestrator (id, name, priority,activity, created_at, updated_at)
+    VALUES (1,"client1", 1, true, NOW(), NOW()),
+           (2,"client2", 2, true, NOW(), NOW());

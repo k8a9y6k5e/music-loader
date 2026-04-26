d435fb3879c81ccb9573f4c3245dae5bde30a8f4
kayke <kaykekk8965@gmail.com>
2026-04-25 22:14:15 -0300

    feat
    add entity of the database of the clients

---
+++ src/main/java/com/krev/musicloader/api/repository/MusicSearchEntity.java
@@ -0,0 +1,34 @@
+package com.krev.musicloader.api.repository;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "musicSearchClient")
+@EntityListeners(AuditingEntityListener.class)
+public class MusicSearchEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "priority", nullable = false, unique = true)
+    private Integer priority;
+
+    @Column(name = "activity", nullable = false)
+    private Boolean activity = true;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
--- src/main/resources/OrchestratorSeed.sql
+++ src/main/resources/OrchestratorSeed.sql
@@ -1,3 +1,3 @@
-INSERT INTO orchestrator (id, name, priority,activity, created_at, updated_at)
+INSERT INTO musicSearchClient (id, name, priority,activity, created_at, updated_at)
     VALUES (1,"client1", 1, true, NOW(), NOW()),
            (2,"client2", 2, true, NOW(), NOW());

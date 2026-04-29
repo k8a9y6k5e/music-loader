c6d86659316b2c48d3f69b2f67a24072f27824de
kayke <kaykekk8965@gmail.com>
2026-04-28 21:47:06 -0300

    fix
    correction in search music dto

--- src/main/java/com/krev/musicloader/api/service/dto/MusicSearchDto.java
+++ src/main/java/com/krev/musicloader/api/service/dto/MusicSearchDto.java
@@ -1,16 +1,3 @@
 package com.krev.musicloader.api.service.dto;
 
-import lombok.Data;
-
-@Data
-public class MusicSearchDto {
-    private String url;
-    private String artists;
-    private String name;
-
-    public MusicSearchDto(String url, String artists, String name) {
-        this.url = url;
-        this.artists = artists;
-        this.name = name;
-    }
-}
+public record MusicSearchDto(String url, String artists, String name) {}

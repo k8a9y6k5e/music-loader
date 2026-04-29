f6e65c8c017fd0d4b3f408f99a98042422b976c7
kayke <kaykekk8965@gmail.com>
2026-04-28 21:45:36 -0300

    feat
    add the function to list the musics in interface

--- src/main/java/com/krev/musicloader/api/client/MusicApiClient.java
+++ src/main/java/com/krev/musicloader/api/client/MusicApiClient.java
@@ -2,6 +2,10 @@ package com.krev.musicloader.api.client;
 
 import com.krev.musicloader.api.service.dto.MusicSearchDto;
 
+import java.util.List;
+
 public interface MusicApiClient {
-    public MusicSearchDto searchMusic(String name);
+    public MusicSearchDto searchMusic(String name, Integer track);
+
+    public List<MusicSearchDto> listMusic(String name);
 }
